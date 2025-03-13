dd26c915edeba672c85d9eade3e76fd0f9591f29
prashant <prashantchouhan711@gmail.com>
2025-03-13 17:54:53 +0530

    sign inlogo fixed

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -62,7 +62,9 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+<div className="h-32 w-full md:w-[70%]">
+<img src="/assets/images/logo.svg" alt="logo" className="h-full w-full object-cover" />
+</div>
         <h2 className="h3-bold md:h2-bold pt-5">Log in to your account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
           welcome back!! enter your details{" "}
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -64,8 +64,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
-        <h2 className="h3-bold md:h2-bold pt-5">Create a new Account</h2>
+ <h2 className="h3-bold md:h2-bold pt-5">Create a new Account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
           to use lume,please enter your account details
         </p>
